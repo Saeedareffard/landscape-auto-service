ac7a93563cd03a3514064bef10859a49b7c659ee
s.areffard <saeedareffard@yahoo.com>
2022-10-17 10:49:11 +1300

    test api

--- js/main.js
+++ js/main.js
@@ -55,7 +55,7 @@
     //   }
     // );
     $.ajax({
-      url: `https://auto-services.onrender.com/email?firstName=${firstName}&carName=${carName}&details=${details}&serviceName=${serviceName}&receivingAddress=${emailAddress}&date=${date}&phone=${phone}`,
+      url: `http://localhost:10000/email?firstName=${firstName}&carName=${carName}&details=${details}&serviceName=${serviceName}&receivingAddress=${emailAddress}&date=${date}&phone=${phone}`,
       data: {},
       type: "GET",
       beforeSend: function (xhr) {
